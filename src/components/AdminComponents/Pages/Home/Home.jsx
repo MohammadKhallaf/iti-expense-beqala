606726e66902574a4ed27ca05846dde7193dd7f5
MohamedMagdi0 <engmagdi0@gmail.com>
2022-02-14 20:58:53 +0200

    admin dash board

--- src/components/AdminComponents/Pages/Home/Home.jsx
+++ src/components/AdminComponents/Pages/Home/Home.jsx
@@ -5,9 +5,11 @@ import {userData} from "./DummyData";
 import WidgetLg from "../../WidgetLg/WidgetLg";
 import WidgetSm from "../../WidgetSm/WidgetSm";
 import SideBar from "../../SideBar/SideBar";
-
+import Navbar from "../../Navbar/Navbar"
 export default function Home() {
     return (
+        <>
+        <Navbar/>
         <div className="home">
              <SideBar />
              <div className="container">
@@ -19,5 +21,6 @@ export default function Home() {
         </div>
             </div>
         </div>
+        </>
     )
 }
