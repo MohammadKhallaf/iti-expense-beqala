302c6deee588e9016ebc155d400b5ce0e1f7a3a9
Mohammed Khallaf <mohammed.khallaf.1997@gmail.com>
2022-02-14 15:02:26 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import Navbar from './components/Navbar';
-import Popup from './components/Login';
-import Hero from './pages/hero/Hero';
-import Stores from './pages/stores/Stores';
-import Register from './pages/register/Register';
-import NewPartner from './components/NewPartner';
+
 import Partner from './pages/partner/Partner';
 import './App.css'
 
--- src/index.js
+++ src/index.js
@@ -4,5 +4,25 @@ import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Hero from "./pages/hero/Hero";
+import Register from "./pages/register/Register";
+import Partner from "./pages/partner/Partner";
+import Stores from "./pages/stores/Stores";
+import Login from "./components/Login";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Hero/>} />
+      <Route path="/register" element={<Register/>} />
+      <Route path="/partner" element={<Partner/>} />
+      <Route path="/stores" element={<Stores/>} />
+      <Route path="/login" element={<Login/>} />
+    </Routes>
+  </BrowserRouter>
+
+    
+    
+    , document.getElementById("root"));
