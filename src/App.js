dd479eb845823ec9b4d8c5f7dd60dff9d6bee8a9
MennaEmadZ <menna.e.zahran@gmail.com>
2022-02-16 21:05:24 +0200

    Register

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hero from "./pages/hero/Hero";
 import Register from "./pages/register/Register";
 import Stores from "./pages/stores/Stores";
 import Login from "./components/Login";
+import Product from "./pages/product/Product";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -15,6 +16,7 @@ function App() {
           <Route path="/partner"exact element={<Partner />} />
           <Route path="/stores"exact element={<Stores />} />
           <Route path="/login"exact element={<Login />} />
+          <Route path="/product"exact element={<Product />} />
         </Routes>
       </Router>
   );
