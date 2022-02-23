9ad5e1b9be1e3098e85e5397394236b6f9e9e770
Mohammed Khallaf <mohammed.khallaf.1997@gmail.com>
2022-02-23 05:02:48 +0200

    minor code format edits

--- src/components/user-dashboard/SectionCard.js
+++ src/components/user-dashboard/SectionCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Stack, Col } from "react-bootstrap";
-import React from "react";
+
 const SectionCard = (props) => {
   return (
     <Col xs="12" lg="6" as="section">
