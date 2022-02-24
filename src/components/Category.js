d307315440ef71a1b752c4836b841731cad9924d
Mohammed Khallaf <mohammed.khallaf.1997@gmail.com>
2022-02-24 17:48:45 +0200

    add filters to products view

--- src/components/Category.js
+++ src/components/Category.js
@@ -5,16 +5,23 @@ import Product from "./../pages/product/Product.css";
 import { backendAPI } from "../store";
 import { useDispatch } from "react-redux";
 import { ADD_ITEM } from "../redux/actions/types";
-import FallbackImage from "./../files/market.png"
+import FallbackImage from "./../files/market.png";
 const Category = (props) => {
   const dispatch = useDispatch();
 
   const [data, setData] = useState(props.storeData);
+  const [filtered, setFilter] = useState(props.storeData);
+
+  /**
+   * 
+   * @param  catItem category name
+   */
   const filterResult = (catItem) => {
-    const result = props.storeData.filter((curData) => {
-      return curData.category === catItem;
+    const result = props.storeData.filter(
+      (currentData) => {
+      return currentData.product.category.name == catItem;
     });
-    setData(result);
+    setFilter(result);
   };
   /**
    * add to cart
@@ -33,10 +40,10 @@ const Category = (props) => {
     //TODO:
     backendAPI
       .post("cart/", {
-        user_id: 1,
-        store_id: 5,
-        product_id: values.id, //!hard coded
-        quantity: 1,
+        user_id: 1, //!hard coded
+        store_id: values.store_id, 
+        product_id: values.id, 
+        quantity: 1, //!hard coded
       })
       .then((response) => {
         console.log(response);
@@ -44,11 +51,11 @@ const Category = (props) => {
       })
       .catch((error) => console.log(error));
   };
-// app -> render pages -> request |-> re render
-useEffect(()=>{
-  setData(props.storeData)
-},[props.storeData])
-
+  // app -> render pages -> request |-> re render
+  useEffect(() => {
+    setData(props.storeData);
+    setFilter(props.storeData)
+  }, [props.storeData]);
 
   return (
     <>
@@ -57,7 +64,29 @@ useEffect(()=>{
         <div className="row mt-5 mx-2">
           <div className="col-md-3">
             <div className="row">
+              {
+                (data.map(item=>item.product.category.name))
+                .filter((name,index,array)=>{
+                  console.log(array,index,name)
+                  return array.indexOf(name) === index;
+                })
+                .map(name=>
+                  <button
+                className="btn btn-outline-success mb-4"
+                onClick={() => filterResult(name)}
+              >
+                {name}
+              </button>
+                  
+                  )
+              }
               <button
+                className="btn btn-outline-success mb-4"
+                onClick={() => setFilter(props.storeData)}
+              >
+                All
+              </button>
+              {/* <button
                 className="btn btn-outline-success mb-4"
                 onClick={() => filterResult("Fruits")}
               >
@@ -86,44 +115,56 @@ useEffect(()=>{
                 onClick={() => setData(props.storeData)}
               >
                 All
-              </button>
+              </button> */}
             </div>
           </div>
           <div className="col-md-9">
             <div className="row ">
-              {data.map((values, index) => {
-                const { id:product_price_id,product:{id:product_id,name:title,description:discreption,img} , price,  store:store_id } = values;
+              {filtered.map((values, index) => {
+                const {
+                  id: product_price_id,
+                  product: {
+                    id: product_id,
+                    name: title,
+                    description: discreption,
+                    img,
+                  },
+                  price,
+                  store: store_id,
+                } = values;
                 return (
-                  
-                    <div className="col-md-6 col-lg-4 me-3" key={index}>
-                      <div
-                        className="card m-2"
-                        style={{ width: "15rem", height: "28rem" }}
-                        id="card"
-                      >
-                        <img src={img || FallbackImage } className="card-img-top" alt="..." />
-                        <div className="card-body">
-                          <h5 className="card-title">{title}</h5>
-                          <p>Price: {price}</p>
-                          <p className="card-text">{discreption}</p>
-                          <div id="bttn">
-                            <button className="btn btn-light">
-                              <i className="fa-fw far fa-eye"></i>
-                            </button>
-                            <button
-                              className="btn btn-light"
-                              onClick={addToCartHandler.bind(this, values)}
-                            >
-                              <i className="fas fa-cart-arrow-down"></i>
-                            </button>
-                            <button className="btn btn-light">
-                              <i className="far fa-heart"></i>
-                            </button>
-                          </div>
+                  <div className="col-md-6 col-lg-4 me-3" key={index}>
+                    <div
+                      className="card m-2"
+                      style={{ width: "15rem", height: "28rem" }}
+                      id="card"
+                    >
+                      <img
+                        src={img || FallbackImage}
+                        className="card-img-top"
+                        alt="..."
+                      />
+                      <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p>Price: {price}</p>
+                        <p className="card-text">{discreption}</p>
+                        <div id="bttn">
+                          <button className="btn btn-light">
+                            <i className="fa-fw far fa-eye"></i>
+                          </button>
+                          <button
+                            className="btn btn-light"
+                            onClick={addToCartHandler.bind(this, values)}
+                          >
+                            <i className="fas fa-cart-arrow-down"></i>
+                          </button>
+                          <button className="btn btn-light">
+                            <i className="far fa-heart"></i>
+                          </button>
                         </div>
                       </div>
                     </div>
-                  
+                  </div>
                 );
               })}
             </div>
