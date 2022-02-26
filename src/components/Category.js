0193aaddb1b81ab9a461191967977f5a8b564996
MohamedAttia <mohamed.attia2016956.com>
2022-02-26 17:02:08 +0200

    storeFilter

--- src/components/Category.js
+++ src/components/Category.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Categories from "./Categories";
 import Product from "./../pages/product/Product.css";
 import { backendAPI } from "../store";
+import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { ADD_ITEM } from "../redux/actions/types";
 import FallbackImage from "./../files/market.png";
@@ -23,6 +24,7 @@ const Category = (props) => {
     });
     setFilter(result);
   };
+  
   // const Category = (props) => {
   //   const dispatch = useDispatch();
   
@@ -132,38 +134,30 @@ const Category = (props) => {
           <div className="col-md-9">
             <div className="row ">
               {filtered.map((values, index) => {
+                
                 const {
                   id: product_price_id,
                   product: {
                     id: product_id,
                     name: title,
                     description: discreption,
-                    img,
+                    image:img,
                   },
                   price,
                   store: store_id,
                 } = values;
                 return (
-                  <div className="col-md-6 col-lg-4 me-3" key={index}>
-                    <div
-                      className="card m-2"
-                      style={{ width: "15rem", height: "28rem" }}
-                      id="card"
-                    >
-                      <img
-                        src={img || FallbackImage}
-                        className="card-img-top"
-                        alt="..."
-                      />
-                      <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
+                  <Link className="col-lg-4 col-md-8 col-sm-8  cardsGrid " to="#" style={{ textDecoration: 'none' }} key={index}>
+                  <div >
+                    <div className="card my-3 storeCard" style={{ width: "15rem" }} >
+                      <img src={ img || FallbackImage } className="card-img-top background" alt="..." style={{ height:"20rem" }}/>
+                      <div className="card-body cardTitle">
+                        <p className="card-title  cardTitle">{title}</p>
+                      </div>
+                      <div>
                         <p>Price: {price}</p>
                         <p className="card-text">{discreption}</p>
-                        <div id="bttn">
-                          <button className="btn btn-light">
-                            <i className="fa-fw far fa-eye"></i>
-                          </button>
-                          <button
+                        <button
                             className="btn btn-light"
                             onClick={addToCartHandler.bind(this, values)}
                           >
@@ -172,10 +166,42 @@ const Category = (props) => {
                           <button className="btn btn-light">
                             <i className="far fa-heart"></i>
                           </button>
-                        </div>
                       </div>
                     </div>
                   </div>
+                </Link>
+                  // <div className="col-md-6 col-lg-4 me-3" key={index}>
+                  //   <div
+                  //     className="card m-2"
+                  //     style={{ width: "15rem", height: "28rem" }}
+                  //     id="card"
+                  //   >
+                  //     <img
+                  //       src= {img || FallbackImage }
+                  //       className="card-img-top"
+                  //       alt="..."
+                  //     />
+                  //     <div className="card-body">
+                  //       <h5 className="card-title">{title}</h5>
+                  //       <p>Price: {price}</p>
+                  //       <p className="card-text">{discreption}</p>
+                  //       <div id="bttn">
+                  //         {/* <button className="btn btn-light">
+                  //           <i className="fa-fw far fa-eye"></i>
+                  //         </button> */}
+                  //         <button
+                  //           className="btn btn-light"
+                  //           onClick={addToCartHandler.bind(this, values)}
+                  //         >
+                  //           <i className="fas fa-cart-arrow-down"></i>
+                  //         </button>
+                  //         <button className="btn btn-light">
+                  //           <i className="far fa-heart"></i>
+                  //         </button>
+                  //       </div>
+                  //     </div>
+                  //   </div>
+                  // </div>
                 );
               })}
             </div>
--- src/components/Location.js
+++ src/components/Location.js
@@ -10,7 +10,7 @@ export default function Location() {
         <div className="row text-start justify-content-center py-5 " >
             <h2 className="fw-bold " >You can find us in </h2>
 
-            <LocationCard name={"alex"} loc={require("./../files/Location/alex.jpg")} />
+            <LocationCard name={"alexandria"} loc={require("./../files/Location/alex.jpg")} />
             <LocationCard name={"cairo"} loc={require("./../files/Location/cairo.jpg")}  />
        
         </div>
