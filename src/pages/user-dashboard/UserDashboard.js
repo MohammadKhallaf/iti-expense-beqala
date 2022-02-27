591b0af1fac8fa6d2364dc84944d29255712690c
Mohammed Khallaf <mohammed.khallaf.1997@gmail.com>
2022-02-27 15:43:05 +0200

    edit user dashboard

--- src/pages/user-dashboard/UserDashboard.js
+++ src/pages/user-dashboard/UserDashboard.js
@@ -1,40 +1,27 @@
 import React, { useState } from "react";
 import { Container } from "react-bootstrap";
-import Categories from "../../components/user-dashboard/categories/categories/Categories";
-import UserCard from "../../components/user-dashboard/others/UserCard";
-import TransFilters from "../../components/user-dashboard/transactions/transactions/TranFilters";
-import Transactions from "../../components/user-dashboard/transactions/transactions/Transactions";
-import SideDetails from "../../components/user-dashboard/others/SideDetails";
+
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 
 import { Doughnut } from "react-chartjs-2";
 import Sidebar from "../../components/user-dashboard/UI/Sidebar";
-import { Routes } from "react-router-dom";
-ChartJS.register(ArcElement, Tooltip, Legend);
-
+import { Outlet } from "react-router-dom";
+import Test from "../../test/pages/Test";
+import UserAccount from "./UserAccount";
+import styles from "./UserDashboard.module.css"
 const UserDashboard = () => {
-  const [chartData, setChartData] = useState({
-    labels: ["Coffe", "Paper", "Fruits", "Cheese"],
-    datasets: [
-      {
-        label: "Months",
-        data: [300, 60, 50, 50],
-        backgroundColor: [
-          "#B21F00",
-          "#C9DE00",
-          "#2FDE00",
-          "#00A6B4",
-          "#6800B4",
-        ],
-      },
-    ],
-  });
+ 
   return (
     <>
       <Sidebar />
-      <Routes></Routes>
+      
+      <Container className={styles.page__container}>
+      <Container as="section" className="p-5 m-auto">
+      <Outlet />
+      </Container>
+      </Container>
 
-      <Container className="w-75 py-5">
+      {/* <Container className="w-75 py-5">
         <h2 className="pt-3">Overview</h2>
         <UserCard />
         <h2 className="pt-3">Categories</h2>
@@ -51,7 +38,7 @@ const UserDashboard = () => {
         <Container fluid className="mt-4 shadow-sm p-5">
           <Doughnut data={chartData} options={{}} />
         </Container>
-      </Container>
+      </Container> */}
     </>
   );
 };
